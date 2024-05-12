0f2589f4c3a188cb8695eff6d7840a2865998cfc
Rene Manuel Orta Lugo <87352628+ReneOrta@users.noreply.github.com>
2024-05-12 15:03:43 -0600

    First commit

---
+++ mx.edu.itcm.businessentities/src/main/java/mx/edu/itcm/businessentities/dto/VendorDtoResponse.java
@@ -0,0 +1,33 @@
+package mx.edu.itcm.businessentities.dto;
+
+import java.io.Serializable;
+
+public class VendorDtoResponse extends VendorDto implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private int businessEntityID;
+	
+	public VendorDtoResponse(String accountNumber, String name, Integer id ) {
+		super(accountNumber, name);
+		this.businessEntityID=id;
+	}
+	
+	public VendorDtoResponse() {
+		super();
+	}
+
+	public int getBusinessEntityID() {
+		return businessEntityID;
+	}
+
+	public void setBusinessEntityID(int businessEntityID) {
+		this.businessEntityID = businessEntityID;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+}
---
+++ mx.edu.itcm.businessentities/src/test/java/mx/edu/itcm/mx/edu/itcm/businessentities/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.itcm.mx.edu.itcm.businessentities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
